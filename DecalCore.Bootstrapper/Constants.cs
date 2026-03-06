d3af6e2ebce443b5458b6c08739b123b400e60af
DecalFree <DecalFree@yahoo.com>
2026-03-06 17:41:24 -0600

    Remove DecalCore.Bootstrapper

--- DecalCore.Bootstrapper/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DecalCore.Bootstrapper;
-
-internal class Constants {
-    public const string Name = "DecalCore Bootstrapper";
-    public const string Version = "1.0.0";
-    public const string Author = "DecalFree";
-}
